4894fc4d1fa6d1bb1249b6b679724930a8eff8e1
mattperkins <github@mattperkins.com>
2019-01-20 10:43:17 +0000

    init

---
+++ src/Swiper.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import Swipeable from 'react-swipeable'
+
+export default class Swiper extends React.Component {
+
+  swiping(e, deltaX, deltaY, absX, absY, velocity) {
+    console.log("You're Swiping...", e, deltaX, deltaY, absX, absY, velocity)
+  }
+
+  swipingLeft(e, absX) {
+    console.log("You're Swiping to the Left...", e, absX)
+  }
+
+  swiped(e, deltaX, deltaY, isFlick, velocity) {
+    console.log("You Swiped...", e, deltaX, deltaY, isFlick, velocity)
+  }
+
+  swipedUp(e, deltaY, isFlick) {
+    console.log("You Swiped Up...", e, deltaY, isFlick)
+  }
+
+  render() {
+    return (
+      <Swipeable
+        onSwiping={this.swiping}
+        onSwipingLeft={this.swipingLeft}
+        onSwiped={this.swiped}
+        onSwipedUp={this.swipedUp} >
+          You can swipe here!
+      </Swipeable>
+    )
+  }
+}
